638befddb7cd5e526423c2b8bdfb315836040fd8
Koros <korostv@proton.me>
2024-11-21 15:18:54 -0500

    Removed provider to make it even more minimal

--- src/routes/plugin@auth.ts
+++ src/routes/plugin@auth.ts
@@ -1,8 +1,7 @@
 import { QwikAuth$ } from "@auth/qwik";
-import Google from "@auth/qwik/providers/google";
 
 export const { onRequest, useSession, useSignIn, useSignOut } = QwikAuth$(
   () => ({
-    providers: [Google],
+    providers: [],
   }),
 );
